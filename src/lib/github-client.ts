90f671e0746dec0897c2c518c500b8e1fd1f50cb
Nikoyaya <58239518+Nikoyaya@users.noreply.github.com>
2026-02-12 04:15:18 +0800

    fix:/符号可能会被过度解析为%2

--- src/lib/github-client.ts
+++ src/lib/github-client.ts
@@ -158,7 +158,7 @@ export async function createCommit(token: string, owner: string, repo: string, m
 }
 
 export async function updateRef(token: string, owner: string, repo: string, ref: string, sha: string, force = false): Promise<void> {
-	const res = await fetch(`${GH_API}/repos/${owner}/${repo}/git/refs/${encodeURIComponent(ref)}`, {
+	const res = await fetch(`${GH_API}/repos/${owner}/${repo}/git/refs/heads/${encodeURIComponent(ref)}`, {
 		method: 'PATCH',
 		headers: {
 			Authorization: `Bearer ${token}`,
